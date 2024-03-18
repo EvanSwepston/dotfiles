0db46f3bace7cd986f801f7eb7813b09f6cc05e5
Evan Swepston <swepston.2@wright.edu>
2024-03-18 11:58:05 -0400

    Added settings for git config and gitignore, aliases for ls, .., and da, created .ssh, and added Vundle with plugins

--- install.sh
+++ install.sh
@@ -27,9 +27,49 @@ if [[ -n "$(which apt)" ]]; then
     else
         curl -O https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh
     fi
+    # https://docs.anaconda.com/free/anaconda/install/linux/
     bash Anaconda3-2024.02-1-Linux-x86_64.sh -b -p /home/$ME/Anaconda3
     echo "PATH=$PATH:/home/kduncan/anaconda3/bin" >> /home/$ME/.profile
-    # https://docs.anaconda.com/free/anaconda/install/linux/
+    # https://git-scm.com/book/sv/v2/Customizing-Git-Git-Configuration
+    git config --global user.name "Evan Swepston"
+    git config --global user.email swepston.2@wright.edu
+    git config --global core.editor vim
+    # https://github.com/github/gitignore/blob/main/Global/Linux.gitignore
+    echo ".fuse_hidden*
+    .directory
+    .Trash-*
+    .nfs*" >> ~/.gitignore_global
+    git config --global core.excludesfile ~/.gitignore_global
+    git config --global help.autocorrect 20
+    ln -s /home/$ME/dotfiles/gitfiles/.gitconfig /home/$ME/.gitconfig
+    alias ls="ls -lah" >> ~/.bashrc
+    alias ..="cd .." >> ~/.bashrc
+    alias da='date "+%Y-%m-%d %A %T %Z"'
+    ln -s /home/$ME/dotfiles/bashrc /home/$ME/.bashrc
+    if [ ! -d "$~/.ssh" ]; then
+        mkdir .ssh
+    fi
+    echo "Host fry
+        User w248exs
+        HostName fry.cs.wright.edu
+        Port 22" >> .ssh/config
+    ln -s /home/$ME/dotfiles/sshfiles/authorized_keys /home/$ME/.ssh/authorized_keys
+    ln -s /home/$ME/dotfiles/sshfiles/config /home/$ME/.ssh/config
+    # https://www.linuxandubuntu.com/home/installing-vundle-the-plugin-manager-for-vim
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    echo "set nocompatible
+    filetype off
+    set rtp+=$ME/.vim/bundle/Vundle.vim
+    call vundle#begin()
+    Plugin 'VundleVim/Vundle.vim'
+    Plug 'sts10/vim-pink-moon'
+    call vundle#end()
+    filetype plugin indent on" >> .vimrc
+    vim +PluginInstall +qall
+    mkdir -p ~/.vim/pack/tpope/start
+    cd ~/.vim/pack/tpope/start
+    git clone https://tpope.io/vim/fugitive.git
+    vim -u NONE -c "helptags fugitive/doc" -c q
 else
     echo "apt is not installed"
     exit 1
